c1db754fcdf1d11c4fcfc9e95ad5a4a2f8acdd85
Leandro Guarino <le.guarino@gmail.com>
2020-08-29 12:14:56 -0300

    Código da aula

---
+++ src/main/java/aula01_vetor_codigo/Main.java
@@ -0,0 +1,22 @@
+package aula01_vetor_codigo;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] vetorzao = new int[10];
+        vetorzao[0] = 3;
+        vetorzao[1] = 4;        
+        vetorzao[3] = 13;
+        vetorzao[2] = 8;
+        vetorzao[5] = 51;
+        vetorzao[4] = 22;
+        vetorzao[6] = 33;
+        System.out.println("Primindo...");
+        
+        for(int i = 0; i < vetorzao.length; i++){
+            System.out.println(vetorzao[i]);
+        }
+        
+        System.out.println("Até a próxima aula");
+        
+    }
+}
